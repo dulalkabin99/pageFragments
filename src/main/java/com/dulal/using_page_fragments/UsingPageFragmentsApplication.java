2aaa70087333beeffe2566c3895f5807c4f1297f
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-27 15:41:45 -0400

    page fragments

---
+++ src/main/java/com/dulal/using_page_fragments/UsingPageFragmentsApplication.java
@@ -0,0 +1,12 @@
+package com.dulal.using_page_fragments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingPageFragmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingPageFragmentsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dulal/using_page_fragments/UsingPageFragmentsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dulal.using_page_fragments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsingPageFragmentsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
